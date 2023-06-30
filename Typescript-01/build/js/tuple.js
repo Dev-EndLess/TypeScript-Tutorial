958d65bffc0a4ad62de7573e40e2a31ebc9a9c96
Fabrizio Pozzi <fabrizio.pozzi@netstrada.it>
2023-06-30 15:47:34 +0200

    tuple example files added

---
+++ Typescript-01/build/js/tuple.js
@@ -0,0 +1,11 @@
+"use strict";
+/* Tuple */
+// the destructuring variables get the types of the corresponding tuple elements
+let myTuple = ['Dave', 42, true];
+let mixed = ['John', 1, false];
+myTuple[1] = 42;
+myTuple[0] = 'Joe';
+myTuple[2] = false;
+mixed[0] = 2;
+mixed[2] = 3;
+mixed[2] = 'Fabri';
